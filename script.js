024a8335b7c184514075b9377fc2f4c62acb53ca
lufayec <lufayeicode@gmail.com>
2024-04-20 10:27:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
-//const music = new Audio("minecraft.mp3");
-//minecraft.loop = true;
-var wind = document.getElementById("wind.mp3").autoplay;
+const music = new Audio("wind.mp3");
+wind.loop = true;
+window.addEventListener("mousemove",()=>{
+    wind.play();
+});
+//var wind = document.getElementById("wind.mp3").autoplay;
