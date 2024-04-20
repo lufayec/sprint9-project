5c85bb9eb20022c75d81e84d85a39319cee4796f
lufayec <lufayeicode@gmail.com>
2024-04-20 11:24:17 -0500

    s

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 
 const music = new Audio("wind.mp3");
 wind.loop = true;
-window.addEventListener("mousemove",()=>{
-    wind.play();
-});
+//window.addEventListener("mousemove",()=>{
+  //  wind.play();
+//});
 //var wind = document.getElementById("wind.mp3").autoplay;
+function playSound(){
+    wind.play()
+}
